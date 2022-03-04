a35a244238a4a9277d6faf8eeac000fa1c402e86
Waruta <62998111+WarutaShinken@users.noreply.github.com>
2022-03-04 15:27:47 +1100

    Dynamic Staking Info

--- staidelta/cmds/farm.py
+++ staidelta/cmds/farm.py
@@ -49,11 +49,52 @@ def farm_cmd() -> None:
     default=None,
     show_default=True,
 )
+@click.option(
+    "-sa",
+    "--staking-addresses",
+    help=(
+        """\b
+        Fetch staking addresses and cache them to
+        always be shown when running 'farm summary'.
+        You must rerun this command option when
+        adding/removing keys or plots to update the
+        cache, even across restarts.
+        
+        \b
+        n - Clear staking address cache.
+        k - Fetch staking addresses from keys. Does
+            not show plot counts.
+        p - Fetch staking addresses from plots. Does
+            not show staking addresses for keys
+            without plots.
+        a - Fetch staking addresses from keys and
+            plots. Shows plot counts and addresses
+            for keys without plots."""
+    ),
+    type=str,
+    default=None,
+    show_default=True,
+)
+@click.option(
+    "-sb",
+    "--staking-balance",
+    help=(
+        """\b
+        Show the balance of each staking address.
+        Requires staking info to be cached by
+        -sa/--staking-addresses beforehand."""
+    ),
+    is_flag=True,
+    type=bool,
+    default=False
+)
 def summary_cmd(
     rpc_port: Optional[int],
     wallet_rpc_port: Optional[int],
     harvester_rpc_port: Optional[int],
     farmer_rpc_port: Optional[int],
+    staking_addresses: Optional[str],
+    staking_balance: Optional[bool],
 ) -> None:
     from .farm_funcs import summary
     import asyncio
